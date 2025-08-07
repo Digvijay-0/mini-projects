b573e1806e5fbd6eb9479176319d2b6d951f0a38
Digvijay 0 <digvijay1src@gmail.com>
2025-08-08 00:52:43 +0530

    play your choice based game, 'can customize to increase your adventure'

--- Python/RockPaperScissor.py
+++ Python/RockPaperScissor.py
@@ -1,34 +1,34 @@
 import random
 
 
-weapons = ['Rock', 'Paper', 'Scissor']
+weapons = ["Rock", "Paper", "Scissor"]
 
 while True:
-    user_choice = input('Choose from: Rock - Paper - Scissor : ').strip().title()
+    user_choice = input("Choose from: Rock - Paper - Scissor : ").strip().title()
     bot_choice = random.choice(weapons)
 
-    print('Your choice = ',user_choice, ', Computer choice = ',bot_choice)
+    print("Your choice = ", user_choice, ", Computer choice = ", bot_choice)
 
     if user_choice == bot_choice:
-        print('Both choose same \n Its Draw Boi 🤝')
+        print("Both choose same \n Its Draw Boi 🤝")
 
-    elif user_choice == 'Paper':
-        if bot_choice == 'Rock':
-            print('You win \n will beat you next time😏')
+    elif user_choice == "Paper":
+        if bot_choice == "Rock":
+            print("You win \n will beat you next time😏")
         else:
-            print('You lose✂️ \n😂')
+            print("You lose✂️ \n😂")
 
-    elif user_choice == 'Scissor':
-        if bot_choice == 'Rock':
-            print('You lose🪨\n😂')
+    elif user_choice == "Scissor":
+        if bot_choice == "Rock":
+            print("You lose🪨\n😂")
         else:
-            print('You win \n will beat you next time😏')
+            print("You win \n will beat you next time😏")
 
-    elif user_choice == 'Rock':
-        if bot_choice == 'Paper':
-            print('You lose🧻\n😂')
+    elif user_choice == "Rock":
+        if bot_choice == "Paper":
+            print("You lose🧻\n😂")
         else:
-            print('You win \n will beat you next time😏')
+            print("You win \n will beat you next time😏")
 
     else:
-        print('Chose wisely bot will get angry 👁️👁️')
+        print("Chose wisely bot will get angry 👁️👁️")
--- Python/choose your Adventure game.py
+++ Python/choose your Adventure game.py
@@ -1,9 +1,47 @@
-name = input("Enter your name : ")
+name = input("Enter your name : ").strip().title()
 print("Welcome", name, "to this adventure")
 
 
 def swim_across():
-    q1 = input("")
+
+    q1 = (
+        input(
+            "You cross a river, Your cloths are wet. Soon its gooing to be dusk \n Would you like to move forward or stay till dawn (stay/walk) : "
+        )
+        .lower()
+        .strip()
+    )
+    if q1 == "stay":
+        print("You were poison by snake, \nYou are ☠️")
+
+    elif q1 == "walk":
+        q2 = (
+            input("You see a stranger, \nWould you like to talk with him? (yes/no) : ")
+            .lower()
+            .strip()
+        )
+        if q2 == "yes":
+            q3 = (
+                input(
+                    "Hello whats your name😑 ? \nwill you tell him your real name or not as he seems suspicious \n Enter name you ganna tell him : "
+                )
+                .title()
+                .strip()
+            )
+            if q3 == name:
+                print("Stanger man gives you a Gold coin \n🏆 You Won the Game 🏆")
+
+            else:
+                print("Stranger stabs you 🔪🩸")
+
+        elif q2 == "no":
+            print(
+                "Stranger follows you, You run try to escape but due to continue travelling You are tired..He catches you... \n and stabs you 🔪🩸"
+            )
+        else:
+            print("Not valid option, \nYou Loose☠️")
+    else:
+        print("Not valid option, \nYou Loose☠️")
 
 
 ans = (
@@ -25,7 +63,7 @@ if ans == "right":
     if ans == "walk":
         ans = (
             input(
-                'You are walking for a quite while would you still like to continue or swim across \n "walk"/"swim" : '
+                "You are walking for a quite while would you still like to continue or swim across \n (walk/swim) : "
             )
             .lower()
             .strip()
@@ -44,7 +82,38 @@ if ans == "right":
         print("Not valid option, \nYou Loose☠️")
 
 elif ans == "left":
-    print("")
+    ans1 = (
+        input(
+            "There is a old wooden bridge, would you like to move forward or just go back to main road, \nEnter "
+            "walk"
+            " to move forward or "
+            "back"
+            " to return to main road "
+        )
+        .strip()
+        .lower()
+    )
+    if ans1 == "walk":
+        ans1 = (
+            input(
+                "The bridge start to swing as intensity of wind is increase and one of wooden step breaks, You dont know whether the you will be able to make it \nwould you like to continue or jump in flowing water below (walk/jump) "
+            )
+            .strip()
+            .lower()
+        )
+        if ans1 == "jump":
+            swim_across()
+        elif ans1 == "walk":
+            print(
+                "You foot stuck in one of the plank of bridge, as you try to remove it the bridge ropes breaks \n Your foot is stuck and you smash to a cliff"
+            )
+        else:
+            print("Not valid option, \nYou Loose☠️")
+    elif ans1 == "back":
+        print("You see a small girl crying and you loose")
+
+    else:
+        print("Not valid option, \nYou Loose☠️")
 
 else:
     print("Not valid option, \nYou Loose☠️")
