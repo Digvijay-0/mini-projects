219f039245df093786badcf730ecefb8e9ba37a5
Digvijay 0 <digvijay1src@gmail.com>
2025-08-07 21:41:26 +0530

    game

---
+++ Python/choose your Adventure game.py
@@ -0,0 +1,13 @@
+name = input("Enter your name : ")
+print("Welcome", name, "to this adventure")
+
+ans = input(
+    "you are on dirt road, it has come to an end You can only go left or right. \nWhich way would you choose?"
+).lower()
+
+if ans == 'right':
+ 
+elif ans == 'left':
+ 
+else:
+    print('Not valid option, \nYou Loose☠️')
