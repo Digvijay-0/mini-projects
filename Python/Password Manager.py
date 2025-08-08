9037ad14b521cfb3aa52b4691eeeae1f2ac95077
Digvijay 0 <digvijay1src@gmail.com>
2025-08-09 00:49:36 +0530

    pswd manager

---
+++ Python/Password Manager.py
@@ -0,0 +1,29 @@
+def add():
+    name = input("Name : ")
+    pswd = input("Password : ")
+    with open("password.txt", "a") as p:
+        p.write(name + " | " + pswd + "\n")
+
+
+def view():
+    with open("password.txt", "r") as p:
+        for i in p.readlines():
+            data = i.rstrip()
+            user, password = data.split("|")
+            print("User : ", user, "| Password : ", password)
+
+
+while True:
+    q1 = (
+        input(
+            'Would you like to view or add new password, "q" to quit \n(view/add/q): '
+        )
+        .strip()
+        .lower()
+    )
+    if q1 == "q":
+        break
+    if q1 == "view":
+        view()
+    elif q1 == "add":
+        add()
