f8d2c240b669f9e94fad48c94bbb7e20396bb910
Digvijay 0 <digvijay1src@gmail.com>
2025-08-10 22:03:38 +0530

    just win condition remaining

--- Python/tic tac toe game.py
+++ Python/tic tac toe game.py
@@ -1,1 +1,29 @@
+board = [" " for i in range(9)]
 
+
+def print_board():
+    row1 = "| {} | {} | {} |".format(board[0], board[1], board[2])
+    row2 = "| {} | {} | {} |".format(board[3], board[4], board[5])
+    row3 = "| {} | {} | {} |".format(board[6], board[7], board[8])
+
+    print()
+    print(row1)
+    print(row2)
+    print(row3)
+    print()
+
+
+def player_move(icon):
+    if icon == "x":
+        num = 1
+    elif icon == "o":
+        num = 2
+
+    print("your turn player {}".format(num))
+
+    choice = int(input("select from (1-9): ").strip())
+    if board[choice - 1] == " ":
+        board[choice - 1] = icon
+    else:
+        print()
+        print("enter in valid space")
